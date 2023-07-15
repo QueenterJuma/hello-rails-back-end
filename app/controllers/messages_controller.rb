a85c14a6bff242d24eff2a8531a8e7174debef3c
QueenterJuma <jumahqueenter@gmail.com>
2023-07-14 18:16:26 -0700

    Fixed linters

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
-    def random
-        random_message = Message.order('RANDOM()').first
-        render json: random_message
-    end
+  def random
+    random_message = Message.order('RANDOM()').first
+    render json: random_message
+  end
 end
